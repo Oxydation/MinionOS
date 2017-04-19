6076f1b4ebbc5e3f505fdd10e1957bb8a2ae3233
jpo2433 <jasmin.pogatschneg@students.fhv.at>
2017-04-19 12:11:25 +0200

    pragma without SWI Number

--- minionOS/applications/syscallApi.c
+++ minionOS/applications/syscallApi.c
@@ -8,7 +8,9 @@
 #include "kernel/systemModules/syscalls/arguments.h"
 #include "kernel/systemModules/syscalls/syscallNumbers.h"
 
-#pragma SWI_ALIAS(syscall, LED_SYSCALL);
+#define UNUSED_SWI_NUMBER 0
+
+#pragma SWI_ALIAS(syscall, UNUSED_SWI_NUMBER);
 static int syscall(SysCall_Args args);
 
 int syscalls_enableLed(bool turnOn, int led) {
