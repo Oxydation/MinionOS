a2cf18b0d93c637b32c40103cdda69ddc6559675
sro <sabrina.rosmann@students.fhv.at>
2017-06-21 20:52:52 +0200

    process can be loaded from sd card with only one read buffer

--- minionOS/applications/systemCallApi.c
+++ minionOS/applications/systemCallApi.c
@@ -8,31 +8,31 @@
 static int makeSysCall(SysCallArgs_t args);
 
 void sysCalls_enableLed(bool turnOn, int led) {
-    SysCallArgs_t args = { SYSCALL_LED, turnOn, led };
+    volatile SysCallArgs_t args = { SYSCALL_LED, turnOn, led };
     makeSysCall(args);
 }
 
 int sysCalls_openFile(const char* fileName) {
-    SysCallArgs_t args = { SYSCALL_FILE_OPEN, (int) fileName };
+    volatile SysCallArgs_t args = { SYSCALL_FILE_OPEN, (int) fileName };
     return makeSysCall(args);
 }
 
 int sysCalls_readFile(int fileDescriptor, uint8_t* buffer, unsigned int bufferSize) {
-    SysCallArgs_t args = { SYSCALL_FILE_READ, fileDescriptor, (int) buffer, bufferSize };
+    volatile SysCallArgs_t args = { SYSCALL_FILE_READ, fileDescriptor, (int) buffer, bufferSize };
     return makeSysCall(args);
 }
 
 void sysCalls_writeFile(int fileDescriptor, const uint8_t* buffer, unsigned int bufferSize) {
-    SysCallArgs_t args = { SYSCALL_FILE_WRITE, fileDescriptor, (int) buffer, bufferSize };
+    volatile SysCallArgs_t args = { SYSCALL_FILE_WRITE, fileDescriptor, (int) buffer, bufferSize };
     makeSysCall(args);
 }
 
 void sysCalls_closeFile(int fileDescriptor) {
-    SysCallArgs_t args = { SYSCALL_FILE_CLOSE, fileDescriptor };
+    volatile SysCallArgs_t args = { SYSCALL_FILE_CLOSE, fileDescriptor };
     makeSysCall(args);
 }
 
 const char* sysCalls_readDirectory(const char* directoryName) {
-    SysCallArgs_t args = { SYSCALL_READDIR, (int) directoryName };
+    volatile SysCallArgs_t args = { SYSCALL_READDIR, (int) directoryName };
     return (const char*) makeSysCall(args);
 }
--- minionOS/kernel/systemModules/loader/loader.c
+++ minionOS/kernel/systemModules/loader/loader.c
@@ -16,33 +16,16 @@ int8_t loader_loadProcess(const char* fileName) {
     }
 
     uint8_t buffer[BUFFER_SIZE];
-    uint8_t readBuffer1[1024];
-    uint8_t readBuffer2[1024];
-    uint8_t readBuffer3[1024];
+    uint8_t readBuffer[1024];
     uint32_t nrOfBytesInFile = 0;
     int32_t nrOfBytesRead;
     uint32_t* pBuffer = (uint32_t*)&buffer;
 
     int i = 0;
     do {
-        if (i % 3 == 0) {
-            nrOfBytesRead = sysCalls_readFile(fileHandle, readBuffer1, 1024);
-            if(nrOfBytesRead > 0) {
-                memcpy(pBuffer, readBuffer1, nrOfBytesRead);
-            }
-        } else if (i % 3 == 1) {
-            nrOfBytesRead = sysCalls_readFile(fileHandle, readBuffer2, 1024);
-            if (nrOfBytesRead > 0) {
-                memcpy(pBuffer, readBuffer2, nrOfBytesRead);
-            }
-        } else if (i % 3 == 2) {
-            nrOfBytesRead = sysCalls_readFile(fileHandle, readBuffer3, 1024);
-            if (nrOfBytesRead > 1024) {
-                nrOfBytesRead= 1024;
-            }
-            if (nrOfBytesRead > 0) {
-                memcpy(pBuffer, readBuffer3, nrOfBytesRead);
-            }
+        nrOfBytesRead = sysCalls_readFile(fileHandle, readBuffer, 1024);
+        if (nrOfBytesRead > 0) {
+            memcpy(pBuffer, readBuffer, nrOfBytesRead);
         }
         nrOfBytesInFile += nrOfBytesRead;
         i++;
