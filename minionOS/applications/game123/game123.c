31bbddb4c3575b988943a3c114e470d8e5928fb9
Mathias Nigsch <mathias.nigsch@gmail.com>
2017-06-22 17:56:16 +0200

    Added third moving head

--- minionOS/applications/game123/game123.c
+++ minionOS/applications/game123/game123.c
@@ -14,10 +14,10 @@
 #include "drivers/dmx/tmh7/dmxTmh7.h"
 #include "drivers/dmx/mhx25/dmxMhx25.h"
 
-uint16_t packetSize = 26;
-uint8_t packet[26];
+uint16_t packetSize = 40;
+uint8_t packet[40];
 
-DmxDataTMH7_t g_tmhData[3] = {
+DmxDataTMH7_t g_tmhData_1[3] = {
         { .pan = 0, .tilt = 80, .green = 0, .blue = 0, .red = 255,
           .dimmingIntensity = 80, .colorMacro = 0, .ledCircuit = 255, .speed =
                   50, },
@@ -28,9 +28,20 @@ DmxDataTMH7_t g_tmhData[3] = {
           .dimmingIntensity = 80, .colorMacro = 0, .ledCircuit = 255, .speed =
                   50, } };
 
-DmxDataMhX25_t g_mhxData = { .pan = 255, .tilt = 100, .speed = 10, .dimmer =
-                                     100,
-                             .colour = RainbowClockwise, .gobo = Gobo2Shake,
+DmxDataTMH7_t g_tmhData_2[3] = {
+        { .pan = 0, .tilt = 170, .green = 0, .blue = 0, .red = 255,
+          .dimmingIntensity = 80, .colorMacro = 0, .ledCircuit = 255, .speed =
+                  50, },
+        { .pan = 0, .tilt = 128, .green = 255, .blue = 0, .red = 255,
+          .dimmingIntensity = 80, .colorMacro = 0, .ledCircuit = 255, .speed =
+                  50, },
+        { .pan = 0, .tilt = 80, .green = 0, .blue = 255, .red = 0,
+          .dimmingIntensity = 80, .colorMacro = 0, .ledCircuit = 255, .speed =
+                  50, } };
+
+DmxDataMhX25_t g_mhxData = { .pan = 255, .tilt = 100, .speed = 10,
+                             .dimmer = 100, .colour = RainbowClockwise, .gobo =
+                                     Gobo2Shake,
                              .goboRotation = 240, .shutter = 200 };
 
 DmxDataMhX25_t g_mhxDataReset = { .pan = 0, .tilt = 0, .speed = 190,
@@ -62,7 +73,7 @@ static uint16_t readQuizFile(uint8_t * target)
     {
         minionIO_writeln("Not able to open quiz file.");
         // not able to open file
-        return -1;
+        return 0;
     }
 
     sysCalls_readFile(quizHandle, target, size);
@@ -70,7 +81,7 @@ static uint16_t readQuizFile(uint8_t * target)
 }
 
 static uint8_t parseQuizData(const uint8_t * data, uint16_t amountChars,
-                         QuizQuestion_t * questions)
+                             QuizQuestion_t * questions)
 {
     int currPos = 0;
     int currentQuizQuestion = 0;
@@ -135,7 +146,8 @@ static uint8_t parseQuizData(const uint8_t * data, uint16_t amountChars,
 static void highlightAnswer(uint8_t answerNumber)
 {
     dmx_createMhX25Packet(1, &g_mhxDataReset, &packet);
-    dmx_createTmh7Packet(13, &g_tmhData[answerNumber - 1], &packet);
+    dmx_createTmh7Packet(13, &g_tmhData_1[answerNumber - 1], &packet);
+    dmx_createTmh7Packet(25, &g_tmhData_2[answerNumber - 1], &packet);
     sysCalls_ctrlDmx(&packet, packetSize);
 }
 
@@ -143,18 +155,20 @@ static void resetMovingHeads(void)
 {
     dmx_createMhX25Packet(1, &g_mhxDataReset, &packet);
     dmx_createTmh7Packet(13, &g_tmhDataReset, &packet);
+    dmx_createTmh7Packet(25, &g_tmhDataReset, &packet);
     sysCalls_ctrlDmx(&packet, packetSize);
 }
 
-
-static void discoLight(uint8_t rounds){
+static void discoLight(uint8_t rounds)
+{
     int counter = 0;
     while (counter < rounds)
     {
         g_mhxData.tilt = 90 + rand() % 40;
-        g_mhxData.pan =  rand() % 255;
+        g_mhxData.pan = rand() % 255;
         dmx_createMhX25Packet(1, &g_mhxData, &packet);
-        dmx_createTmh7Packet(13, &g_tmhData[counter % 3], &packet);
+        dmx_createTmh7Packet(13, &g_tmhData_1[rand() % 3], &packet);
+        dmx_createTmh7Packet(25, &g_tmhData_2[rand() % 3], &packet);
         sysCalls_ctrlDmx(&packet, packetSize);
         delay(600000);
         counter++;
@@ -211,28 +225,32 @@ static void playGame(QuizQuestion_t * questions, uint8_t amountOfQuestions)
     }
 }
 
-int game123_main(int argc, char* argv[])
+static void prepareDmx(void)
 {
-    uint8_t quizFile[1400] = { 0 };
-    uint16_t amountChars = readQuizFile(&quizFile);
-
-    if (amountChars == -1)
-    {
-        return 1;
-    }
-
-    uint8_t amountOfQuestions = parseQuizData(&quizFile, amountChars, &g_questions);
     uint8_t i = 0;
     while (i++ < 200)
     {
         delay(2000);
-        //dmx_createMhX25Packet(1, &g_mhxData, &packet);
-        //dmx_createTmh7Packet(13, &g_tmhData[0], &packet);
         sysCalls_ctrlDmx(&packet, packetSize);
     }
+}
+
+int game123_main(int argc, char* argv[])
+{
+    prepareDmx();
+
+    uint8_t quizFile[1400] = { 0 };
+    uint16_t amountChars = readQuizFile(&quizFile);
+
+    if (amountChars == 0)
+    {
+        return 1;
+    }
+
+    uint8_t amountOfQuestions = parseQuizData(&quizFile, amountChars,
+                                              &g_questions);
 
     playGame(&g_questions, amountOfQuestions);
 
     return 0;
 }
-
