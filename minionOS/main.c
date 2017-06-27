a663a55d415c3475a03c26f55c89d8b8e247c997
Arno Riedmann <arno.riedmann@students.fhv.at>
2017-06-27 18:48:51 +0200

    cleanup main removed unused includes removed uartInit (automatically done in vfs_init) removed sdCard_init (automatically done in vfs_init) removed LED DIR (automatically done in led hal) removed LEDON/OFF load (can be done dynamically)

--- minionOS/main.c
+++ minionOS/main.c
@@ -1,26 +1,12 @@
-#include <stdio.h>
 #include <inttypes.h>
-
-#include <kernel/common/mmio.h>
-#include <kernel/devices/omap3530/includes/beagleBoardC4.h>
-#include <kernel/devices/omap3530/includes/modeSwitch.h>
-#include <kernel/hal/gpio/gpio.h>
-#include <kernel/hal/interrupts/interrupts.h>
-#include <kernel/hal/timer/systemTimer.h>
-#include <kernel/hal/timer/timer.h>
-#include <kernel/systemModules/mmu/mmu.h>
-#include "global/types.h"
+#include "kernel/devices/omap3530/includes/modeSwitch.h"
+#include "kernel/hal/interrupts/interrupts.h"
+#include "kernel/hal/timer/systemTimer.h"
+#include "kernel/systemModules/mmu/mmu.h"
 #include "kernel/systemModules/scheduler/scheduler.h"
-#include "kernel/systemModules/processManagement/processManager.h"
 #include "kernel/systemModules/filesystem/vfs.h"
-#include "kernel/hal/mmc_sd/sdCard.h"
-#include "kernel/hal/uart/uart.h"
 #include "kernel/systemModules/loader/loader.h"
 #include "systemCallApi.h"
-
-UartConfig_t g_uartConfig = { .baudMultiple = x16, .baudRate = 115200,
-                                .stopMode = STOP_1, .parityMode = NO_PARITY,
-                                .wordLength = LENGTH_8 };
 
 int main(void)
 {
@@ -30,21 +16,13 @@ int main(void)
 
     interrupts_initIrq();
 
-    // Set output direction
-    gpio_pinMode(GPIO_USR0_LED, OUTPUT);
-    gpio_pinMode(GPIO_USR1_LED, OUTPUT);
-
-    uart_initModule(UART3, g_uartConfig);
-    sdCard_initialize_Ch1();
     vfs_init();
     systemTimer_init(1000);
     scheduler_init();
 
-    sysCalls_enableLed(1, LED_0);
-    sysCalls_enableLed(1, LED_1);
+    sysCalls_enableLed(false, LED_0);
+    sysCalls_enableLed(false, LED_1);
 
-    loader_loadProcess("/LEDON.OUT", ELF);
-    loader_loadProcess("/LEDOFF.OUT", ELF);
     loader_loadProcess("/SHELL.OUT", ELF);
 
     _enable_interrupts();
